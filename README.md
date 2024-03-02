0c0f10416b216160e550d157d57e6cca2409f149
Mohit kumar <128957753+mohitk73@users.noreply.github.com>
2024-03-03 05:20:30 +0530

    Add files via upload
    SmartWork is a cutting-edge platform designed to revolutionize the academic landscape, providing students with the tools they need to excel in their studies effortlessly. Our mission is simple: to streamline the process of creating assignments, practical files, and frontend projects, while fostering a culture of academic excellence and innovation.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# hackathon-smartwork
-This project is useful for all students that are in college as it helps the students as our team will make their assignments at reasonable prices and at quality work at time
+# smartwork.github.io
---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.querySelector('.menu').addEventListener('click', function() {
+    var navbar = document.getElementById('navbar');
+    if (navbar.style.display === 'block') {
+        navbar.style.display = 'none';
+    } 
+    else {
+        navbar.style.display = 'block';
+    }
+});
+
